be0c63e22454b39819ccaa8ba926be7881b5caba
floklein <4128942+floklein@users.noreply.github.com>
2024-08-10 02:30:26 +0200

    excalidraw

--- apps/api/src/rooms/schema/MyRoomState.ts
+++ apps/api/src/rooms/schema/MyRoomState.ts
@@ -1,10 +1,11 @@
 import { Schema, type, MapSchema } from "@colyseus/schema";
 
 export class Player extends Schema {
-  @type("number") x: number = 0.11;
-  @type("number") y: number = 2.22;
+  @type("number") score: number = 0;
 }
 
 export class MyRoomState extends Schema {
   @type({ map: Player }) players = new MapSchema<Player>();
+  @type("number") currentRound: number = 0;
+  @type("number") currentStep: number = 0;
 }
