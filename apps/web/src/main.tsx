dd9f880c4fef551bf7adf446fd87cef9e9300f7b
floklein <4128942+floklein@users.noreply.github.com>
2024-08-20 01:10:28 +0200

    ts

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { initializeI18n } from "./i18n";
 
-await initializeI18n();
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+initializeI18n().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
