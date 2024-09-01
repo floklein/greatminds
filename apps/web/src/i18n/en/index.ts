daa845b5c0604da2d65672898e957b5a7e1d5b67
floklein <4128942+floklein@users.noreply.github.com>
2024-09-02 01:35:55 +0200

    skip round if hinter leaves

--- apps/web/src/i18n/en/index.ts
+++ apps/web/src/i18n/en/index.ts
@@ -1,8 +1,10 @@
+import { errors } from "./errors";
 import { home } from "./home";
 import { room } from "./room";
 import { root } from "./root";
 
 export const en = {
+  errors,
   home,
   room,
   root,
--- apps/web/src/i18n/fr/index.ts
+++ apps/web/src/i18n/fr/index.ts
@@ -1,9 +1,11 @@
+import { errors } from "./errors";
 import { home } from "./home";
 import { range } from "./range";
 import { room } from "./room";
 import { root } from "./root";
 
 export const fr = {
+  errors,
   home,
   range,
   room,
